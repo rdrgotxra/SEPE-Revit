45858b8da7ff1cfb109cc23a134cf57015f64b76
Rodrigo Teixeira <rdrgotxra@gmail.com>
2026-05-14 17:16:02 -0300

    estou debugando

--- startup.py
+++ startup.py
@@ -3,7 +3,7 @@ import subprocess
 try:
     subprocess.Popen(
         ["pyrevit", "extensions", "update", "SEPE-Revit"],
-        creationflags=subprocess.CREATE_NO_WINDOW,
+        creationflags=0x08000000,
     )
 except Exception as e:
     print("ERRO ao iniciar update: {}".format(e))

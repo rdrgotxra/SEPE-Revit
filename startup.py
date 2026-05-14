ce8f0055384377298b86470042118c38a2becd61
Rodrigo Teixeira <rdrgotxra@gmail.com>
2026-05-14 17:32:03 -0300

    estou debugando

--- startup.py
+++ startup.py
@@ -1,9 +1,12 @@
 import subprocess
 
+from pyrevit.loader import sessionmgr
+
 try:
     subprocess.call(
         "pyrevit extensions update SEPE-Revit", shell=True, creationflags=0x08000000
     )
+    sessionmgr.reload_pyrevit()
 
 except Exception as e:
     print("ERRO ao iniciar update: {}".format(e))

2623d28646072bd4690486d43d63d3bb560d20cd
Rodrigo Teixeira <rdrgotxra@gmail.com>
2026-05-14 17:10:53 -0300

    estou debugando

--- startup.py
+++ startup.py
@@ -1,6 +1,10 @@
 import subprocess
 
 try:
-    subprocess.call(["pyrevit", "extensions", "update", "SEPE-Revit"])
+    subprocess.call(
+        ["pyrevit", "extensions", "update", "SEPE-Revit"],
+        shell=True,
+        creationflags=0x08000000,
+    )
 except Exception as e:
     print("ERRO ao iniciar update: {}".format(e))
